0637a6b42807255203692225bf18a7aaa9c7a893
Sasha <nasmo.shit1@yandex.ru>
2026-09-11 10:41:02 +0500

    Initial commit

---
+++ app/src/main/java/com/shitikov/calculator/ui/theme/Theme.kt
@@ -0,0 +1,58 @@
+package com.shitikov.calculator.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    background = Black,
+    primary = Gray400,
+    secondary = Green200,
+    tertiary = Blue200,
+    primaryContainer = Gray500,
+    onPrimaryContainer = White,
+    onSecondaryContainer = Gray100,
+    onBackground = White
+)
+
+private val LightColorScheme = lightColorScheme(
+    background = White,
+    primary = Gray200,
+    secondary = Green100,
+    tertiary = Blue100,
+    primaryContainer = Gray300,
+    onPrimaryContainer = Black,
+    onSecondaryContainer = Black,
+    onBackground = Gray600
+)
+
+@Composable
+fun CalculatorTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = false,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
